e38a15a32cd9082eb68cf05e99eaded8c360b001
Rossenzii <jangmj80@naver.com>
2025-08-17 18:33:01 +0900

    [feat] initial commit

---
+++ src/test/java/mutsa/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package mutsa.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
