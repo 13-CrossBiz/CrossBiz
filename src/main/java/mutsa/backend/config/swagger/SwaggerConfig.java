9253e41258a31aba266e2e1f53c1d1b46eaa788a
Rossenzii <jangmj80@naver.com>
2025-08-19 23:41:31 +0900

    [feat] swagger init

---
+++ src/main/java/mutsa/backend/config/swagger/SwaggerConfig.java
@@ -0,0 +1,19 @@
+package mutsa.backend.config.swagger;
+
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.models.OpenAPI;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+//    @Bean
+//    public OpenAPI openAPI() {
+//        return new OpenAPI()
+//                .info(new Info()
+//                        .title("Visa Recommendation API")
+//                        .description("API 명세서")
+//                        .version("v1.0"));
+//    }
+}
